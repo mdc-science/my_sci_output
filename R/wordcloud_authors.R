83b534ccb85f90586ca91d3147731c316713387c
Daniel Moreira <d.carneiro.moreira@gmail.com>
2024-08-13 19:00:51 -0300

    Apply tidyverse style

--- R/wordcloud_authors.R
+++ R/wordcloud_authors.R
@@ -2,17 +2,19 @@ library(wordcloud)
 library(magick)
 
 #### Word Cloud - Authors ####
-df_authors <- df_all %>% 
-  select(year, title, authors) %>% 
-  separate_wider_delim(authors, delim = ";", too_few = "align_start",
-                       names = c(sprintf("author_%d", seq(1:50))))
+df_authors <- df_all %>%
+  select(year, title, authors) %>%
+  separate_wider_delim(authors,
+    delim = ";", too_few = "align_start",
+    names = c(sprintf("author_%d", seq(1:50)))
+  )
 
-df_authors <- df_authors %>% 
+df_authors <- df_authors %>%
   gather(key = "name", value = "author", -year, -title)
 
-dt_names <- df_authors %>% 
-  group_by(author) %>% 
-  summarize(freq = n()) %>% 
+dt_names <- df_authors %>%
+  group_by(author) %>%
+  summarize(freq = n()) %>%
   rename(word = author) %>%
   filter(!is.na(word)) %>%
   filter(word != "Moreira, D.C.") %>%
@@ -22,18 +24,21 @@ dt_names <- df_authors %>%
 set.seed(123)
 
 # Open PNG graphics device
-if (!dir.exists('wordcloud')) {
-  dir.create('wordcloud')
+if (!dir.exists("wordcloud")) {
+  dir.create("wordcloud")
 }
 
-output_path <- here('wordcloud', paste0(format(Sys.Date(), "%Y.%m.%d"), "_authors_cloud.png"))
+output_path <- here("wordcloud", paste0(format(Sys.Date(), "%Y.%m.%d"), "_authors_cloud.png"))
 
-png(output_path, 
-    width = 20, height = 18, units = 'cm', res = 300)
+png(output_path,
+  width = 20, height = 18, units = "cm", res = 300
+)
 
-wordcloud(words = dt_names$word, freq = dt_names$freq, min.freq = 2,
-          max.words = 150, random.order = FALSE, rot.per=0, scale = c(5, 0.5),
-          colors = rev(brewer.pal(5, "Spectral")))
+wordcloud(
+  words = dt_names$word, freq = dt_names$freq, min.freq = 2,
+  max.words = 150, random.order = FALSE, rot.per = 0, scale = c(5, 0.5),
+  colors = rev(brewer.pal(5, "Spectral"))
+)
 
 # Close the graphics device
 dev.off()
--- R/wordcloud_keywords.R
+++ R/wordcloud_keywords.R
@@ -8,12 +8,14 @@ max_count <- df_all %>%
   summarise(max_count = max(count)) %>%
   pull(max_count)
 
-df_keywords <- df_all %>% 
-  select(year, title, keywords_auth_scopus) %>% 
-  separate_wider_delim(keywords_auth_scopus, delim = ";", too_few = "align_start",
-                       names = c(sprintf("keyword_%d", seq(1:max_count))))
+df_keywords <- df_all %>%
+  select(year, title, keywords_auth_scopus) %>%
+  separate_wider_delim(keywords_auth_scopus,
+    delim = ";", too_few = "align_start",
+    names = c(sprintf("keyword_%d", seq(1:max_count)))
+  )
 
-df_keywords <- df_keywords %>% 
+df_keywords <- df_keywords %>%
   gather(key = "key", value = "keyword", -year, -title)
 
 # Convert the text to lower case
@@ -21,28 +23,30 @@ df_keywords$keyword <- tolower(df_keywords$keyword)
 df_keywords$keyword <- trimws(df_keywords$keyword)
 df_keywords$keyword <- lemmatize_words(df_keywords$keyword)
 
-dt_keywords <- df_keywords %>% 
-  group_by(keyword) %>% 
-  summarize(freq = n()) %>% 
+dt_keywords <- df_keywords %>%
+  group_by(keyword) %>%
+  summarize(freq = n()) %>%
   rename(word = keyword) %>%
   filter(!is.na(word)) %>%
-  filter(word != 'article') %>%
+  filter(word != "article") %>%
   filter(freq > 0)
 
 set.seed(1234)
 
 # Open PNG graphics device
-if (!dir.exists('wordcloud')) {
-  dir.create('wordcloud')
+if (!dir.exists("wordcloud")) {
+  dir.create("wordcloud")
 }
 
 # Save the word cloud to a temporary file
 temp_file <- tempfile(fileext = ".png")
 
-png(temp_file, width = 28, height = 20, units = 'cm', res = 600)
-wordcloud(words = dt_keywords$word, freq = dt_keywords$freq, min.freq = 2,
-          max.words = 150, random.order = FALSE, rot.per = 0, scale = c(4, 0.4),
-          colors = brewer.pal(6, cloud_color))
+png(temp_file, width = 28, height = 20, units = "cm", res = 600)
+wordcloud(
+  words = dt_keywords$word, freq = dt_keywords$freq, min.freq = 2,
+  max.words = 150, random.order = FALSE, rot.per = 0, scale = c(4, 0.4),
+  colors = brewer.pal(6, cloud_color)
+)
 dev.off()
 
 # Load the saved word cloud image with magick and trim the whitespace
@@ -50,5 +54,5 @@ trimmed_image <- image_read(temp_file) %>%
   image_trim()
 
 # Save the trimmed image to the desired output location
-output_file <- here('wordcloud', paste0(format(Sys.Date(), "%Y.%m.%d"), "_keywords_cloud.png"))
+output_file <- here("wordcloud", paste0(format(Sys.Date(), "%Y.%m.%d"), "_keywords_cloud.png"))
 image_write(trimmed_image, path = output_file)
